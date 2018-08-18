9abc5dd385072dfb5a925e6e4ab74220165c8e03
Darnell Lemons <darnelldlemons@gmail.com>
2018-08-18 10:33:23 -0400

    Volunteer Opportunity

---
+++ VMSOpportunity/VMSOpportunity/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VMSOpportunity.Models;
+
+namespace VMSOpportunity.Controllers
+{
+    public class VOController : Controller
+    {
+        public ActionResult Index()
+        {
+            using (VolunteerOpportunityDBContext db = new VolunteerOpportunityDBContext())
+            {
+                return View(db.VO.ToList());
+            }
+        }
+
+        public ActionResult AddOpportunity()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddOpportunity(VolunteerOpportunity vo)
+        {
+            if (ModelState.IsValid)
+            {
+                using (VolunteerOpportunityDBContext db = new VolunteerOpportunityDBContext())
+                {
+                    db.VO.Add(vo);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+
+            }
+            return View();
+
+
+        }
+    }
+}
---
+++ VMSOpportunity/VMSOpportunity/Models/VolunteerOpportunity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace VMSOpportunity.Models
+{
+    public class VolunteerOpportunity
+    {
+        [Key]
+        public String VolunteerID { get; set; }
+
+        [Required(ErrorMessage = "Please give a name of the oppurtunity")]
+        public String Opportunity { get; set; }
+
+        [Required(ErrorMessage = "Please give a quick description of the oppurtunity")]
+        public String Description { get; set; }
+
+        [Required(ErrorMessage = "Please list what centers the oppurtunity will be held at")]
+        public String Center { get; set; }
+
+        [Required(ErrorMessage = "What days will this oppurnity be held on")]
+        public String Days { get; set; }
+
+        [Required(ErrorMessage = "What times will this oppurtunity be held at")]
+        public String Time { get; set; }
+
+    }
+}
